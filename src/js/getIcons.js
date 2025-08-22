ffbcde68725c80d56657e67e42c1e051122eae87
Issa-Abbani <issaabbani04@gmail.com>
2025-08-22 17:20:46 +0300

    Worked around MIME errors

--- src/js/getIcons.js
+++ src/js/getIcons.js
@@ -1,21 +1,12 @@
-import clearDay from '../assets/Weather Icons/clear-day.svg';
-import clearNight from '../assets/Weather Icons/clear-night.svg';
-import cloudyDay from '../assets/Weather Icons/cloudy-day.svg';
-import cloudyNight from '../assets/Weather Icons/cloudy-night.svg';
-import fog from '../assets/Weather Icons/fog.svg';
-import rain from '../assets/Weather Icons/rain.svg';
-import snow from '../assets/Weather Icons/snow.svg';
-import thunder from '../assets/Weather Icons/thunder.svg';
-import wind from '../assets/Weather Icons/wind.svg';
-
 export const weatherIcons = {
-  clearDay,
-  clearNight,
-  cloudyDay,
-  cloudyNight,
-  fog,
-  rain,
-  snow,
-  thunder,
-  wind
+  clearDay: '../assets/Weather_Icons/clear-day.svg',
+  clearNight: '../assets/Weather_Icons/clear-night.svg',
+  cloudyDay: '../assets/Weather_Icons/cloudy-day.svg',
+  cloudyNight: '../assets/Weather_Icons/cloudy-night.svg',
+  fog: '../assets/Weather_Icons/fog.svg',
+  rain: '../assets/Weather_Icons/rain.svg',
+  snow: '../assets/Weather_Icons/snow.svg',
+  thunder: '../assets/Weather_Icons/thunder.svg',
+  wind: '../assets/Weather_Icons/wind.svg'
 };
+
