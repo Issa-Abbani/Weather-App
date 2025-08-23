d640a62a12103ce1bb213456e72b2ad4c0cf6abe
Issa-Abbani <issaabbani04@gmail.com>
2025-08-23 08:26:10 +0300

    Switch to prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development", // use development for webpack serve
+  mode: "production", 
   entry: "./src/js/script.js",
   output: {
     filename: "main.js",
